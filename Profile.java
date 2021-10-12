46d923075bf82b3d25ad7e75e16321a7eb8d21fd
Marsani Idris <marsanix@gmail.com>
2021-10-12 12:01:57 +0700

    initial first

---
+++ Profile.java
@@ -0,0 +1,26 @@
+
+public class Profile {
+
+    public static void main(String args[]) {
+        String nama, alamat, group, namaGroup, posisi;
+        int umur;
+        double tinggi;
+
+        nama = "Marsani";
+        alamat = "Batam Kota";
+        group = "Group 10";
+        namaGroup = "Jagonya Coding";
+        posisi = "Hacker";
+        umur = 20;
+        tinggi = 175.0;
+
+        System.out.println("Nama: " + nama);
+        System.out.println("Umur: " + umur + " Tahun");
+        System.out.println("Tinggi: " + tinggi + "cm");
+        System.out.println("Alamat: " + alamat);
+        System.out.println("Group: " + group);
+        System.out.println("Nama Group: " + namaGroup);
+        System.out.println("Posisi: " + posisi);
+
+    }
+}
